e8b0f6a3cd2a918fcc3dc6805ebc8aad328b95c1
Lizardo23 <velasquezlizardo80@gmail.com>
2025-11-25 06:23:52 -0500

    hola

--- js/primerejemplo.js
+++ js/primerejemplo.js
@@ -2,18 +2,18 @@ console.log("Hola Mundo");
 alert("Hola Mundo, esto es un alert") 
 
 function triste(){
-    document.getElementById('carita').src='images/sad.png';
+    document.getElementById('carita').src='imagenes/sad.png';
     alert("Po que ta triste mano");
 }
 function feliz(){
-    document.getElementById('carita').src='images/happy.png';
+    document.getElementById('carita').src='imagenes/happy.png';
 }
 function cambiarColor(){
     document.getElementById('menu').style.color='#eed33aff';
 
 }
 function cambiarLogo(){
-    document.getElementById('logo').src='images/logo2.jpg';
+    document.getElementById('logo').src='imagenes/logo2.jpg';
 }
 function cambiarFondo(){
      document.getElementById('menu').style.background='#524604';

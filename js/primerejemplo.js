9bc1b977edb59be3c436877742243cc965f3a873
Lizardo23 <velasquezlizardo80@gmail.com>
2025-11-25 06:54:08 -0500

    hola

--- js/primerejemplo.js
+++ js/primerejemplo.js
@@ -1,5 +1,5 @@
 console.log("Hola Mundo");
-alert("Hola Mundo, esto es un alert") 
+alert("Hola Mundo, esto es un alert");
 
 function triste(){
     document.getElementById('carita').src='imagenes/sad.png';
